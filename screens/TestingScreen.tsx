42c6eb41aec0dcfb33f17e721f5c74b6056b9914
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-03-18 13:30:33 +0530

    Tried styling

--- screens/TestingScreen.tsx
+++ screens/TestingScreen.tsx
@@ -1,6 +1,6 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Header} from 'react-native/Libraries/NewAppScreen';
 import {RootStackParamList} from '../types';
@@ -8,14 +8,26 @@ import {RootStackParamList} from '../types';
 const TestingScreen = ({route, navigation}: Props) => {
   return (
     <SafeAreaProvider>
-      <Header />
-      <View>
-        <Text>Hello World</Text>
+      <View style={style.title}>
+        <Text style={style.bigText}>Hello World</Text>
       </View>
     </SafeAreaProvider>
   );
 };
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Testing'>;
+
+const style = StyleSheet.create({
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+
+  bigText: {
+    fontSize: 40,
+  },
+});
 
 export default TestingScreen;
