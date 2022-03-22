1c51c90990b0f51963c30257596f16e77a888925
pranjal jain <pranjaljain584@gmail.com>
2022-03-22 13:12:51 +0530

    ui update

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -20,6 +20,9 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import FontAwesome, { SolidIcons, RegularIcons, BrandIcons } from 'react-native-fontawesome';
+// import MapboxGL from '@react-native-mapbox-gl/maps';
+// import style from '../assets/css/home.css' ;
 
 import {
   Colors,
@@ -30,95 +33,44 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import {RootStackParamList} from '../types';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const HomeScreen = ({route, navigation}: Props) => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>HomeScreen.tsx</Text> to change
-            this screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-          <Button
-            title="Goto Testing"
-            onPress={() => {
-              navigation.navigate('Testing');
-            }}
-          />
-        </View>
-      </ScrollView>
+    <SafeAreaView >
+    <View style={styles.map}>
+
+     <View style={styles.navbar}>
+        <FontAwesome style={styles.ic} icon={SolidIcons.user} />
+        <View> <FontAwesome  /> </View>
+        <Button title='Explore' />
+      </View>
+     </View>
+
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  map : {
+      height :'100%' ,
+      width : '100%' ,
+      display:'flex',
+      flexDirection : 'column' ,
+      justifyContent : 'flex-end' ,
+      backgroundColor : '#0A95FF' ,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  ic : {
+    color : '#EA3924' ,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  navbar: {
+    backgroundColor : '#FFFFFF' ,
+    verticalAlign : 'bottom' ,
+    display:'flex',
+    flexDirection : 'row' ,
+    justifyContent : 'space-between' ,
+    height:'10%' ,
+    width:'100%',
+  }
 });
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
