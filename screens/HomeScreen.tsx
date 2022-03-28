446fe5b89fdcd19fca76de54d892f5f2d7f78688
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-03-28 09:34:28 +0530

    Minor bug fix

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -9,7 +9,7 @@
  */
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React,{useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Button,
   SafeAreaView,
@@ -21,9 +21,9 @@ import {
   View,
   PermissionsAndroid,
   TouchableNativeFeedback,
-    TouchableHighlight,
-    TouchableOpacity,
-    TouchableWithoutFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
 // import style from '../assets/css/home.css' ;
 
@@ -35,140 +35,172 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import {RootStackParamList} from '../types';
-import WifiManager from "react-native-wifi-reborn";
-import { BleManager } from 'react-native-ble-plx';
+import WifiManager from 'react-native-wifi-reborn';
+import {BleManager} from 'react-native-ble-plx';
 import {Icon} from 'react-native-elements';
 
 const requestWifiPermission = async () => {
   try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: "Cool Photo App Camera Permission",
-            message:
-              "Cool Photo App needs access to your camera " +
-              "so you can take awesome pictures.",
-            buttonNeutral: "Ask Me Later",
-            buttonNegative: "Cancel",
-            buttonPositive: "OK"
-          }
-        )
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log("Thank you for your permission! :)");
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Cool Photo App Camera Permission',
+        message:
+          'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Thank you for your permission! :)');
 
-          WifiManager.getCurrentWifiSSID().then(
-            ssid => {
-              console.log("Your current connected wifi SSID is " + ssid);
-            },
-            () => {
-              console.log("Cannot get current SSID!");
-            }
-          );
-          WifiManager.getBSSID().then(
-              bssid => {
-                console.log("Your current connected wifi BSSID is " + bssid);
-              },
-              () => {
-                console.log("Cannot get current BSSSID!");
-              }
-            );
+      WifiManager.getCurrentWifiSSID().then(
+        ssid => {
+          console.log('Your current connected wifi SSID is ' + ssid);
+        },
+        () => {
+          console.log('Cannot get current SSID!');
+        },
+      );
+      WifiManager.getBSSID().then(
+        bssid => {
+          console.log('Your current connected wifi BSSID is ' + bssid);
+        },
+        () => {
+          console.log('Cannot get current BSSSID!');
+        },
+      );
 
-        WifiManager.getCurrentSignalStrength().then(
-          level => {
-            console.log("Your current connected wifi RSSI is " + level);
-          },
-          () => {
-            console.log("Cannot get current RSSI!");
-          }
-        );
-
-        } else {
-          console.log("You will not able to retrieve wifi available networks list");
-        }
-      } catch (err) {
-        console.warn("err:***",err)
-      }
-
-
+      WifiManager.getCurrentSignalStrength().then(
+        level => {
+          console.log('Your current connected wifi RSSI is ' + level);
+        },
+        () => {
+          console.log('Cannot get current RSSI!');
+        },
+      );
+    } else {
+      console.log('You will not able to retrieve wifi available networks list');
+    }
+  } catch (err) {
+    console.warn('err:***', err);
+  }
 };
 
 const HomeScreen = ({route, navigation}: Props) => {
-    const [selected,setSelected] = useState('Indoor') ;
-    const onPressButton = () => {
-        setSelected('Outdoor') ;
-    }
+  const [selected, setSelected] = useState('Indoor');
+  const onPressButton = () => {
+    setSelected('Outdoor');
+  };
   const onPressButton2 = () => {
-      setSelected('Indoor') ;
-  }
+    setSelected('Indoor');
+  };
 
-//   const bluetoothInstance = new BleManager();
+  //   const bluetoothInstance = new BleManager();
 
-//     const scanAndConnect = () => {
-//       bluetoothInstance.startDeviceScan(null, { allowDuplicates: true }, (error, device) => {
-//         console.log('device', device);
-//         console.log('error', error);
-//         if (error) {
-//           // Handle error (scanning will be stopped automatically)
-//           return;
-//         }
-//
-//         console.log("**"+ device?.name) ;
-//         if (device?.name === 'MyProjectName') {
-//           bluetoothInstance.stopDeviceScan();
-//         } else {
-//           // bluetoothInstance.stopDeviceScan();
-//         }
-//       });
-//     };
+  //     const scanAndConnect = () => {
+  //       bluetoothInstance.startDeviceScan(null, { allowDuplicates: true }, (error, device) => {
+  //         console.log('device', device);
+  //         console.log('error', error);
+  //         if (error) {
+  //           // Handle error (scanning will be stopped automatically)
+  //           return;
+  //         }
+  //
+  //         console.log("**"+ device?.name) ;
+  //         if (device?.name === 'MyProjectName') {
+  //           bluetoothInstance.stopDeviceScan();
+  //         } else {
+  //           // bluetoothInstance.stopDeviceScan();
+  //         }
+  //       });
+  //     };
 
-//     useEffect(() => {
-//       bluetoothInstance.onStateChange((state) => {
-//         console.log('state', state);
-//         if (state === 'PoweredOn') {
-//           scanAndConnect();
-//         }
-//       }, true);
-//     }, []);
+  //     useEffect(() => {
+  //       bluetoothInstance.onStateChange((state) => {
+  //         console.log('state', state);
+  //         if (state === 'PoweredOn') {
+  //           scanAndConnect();
+  //         }
+  //       }, true);
+  //     }, []);
 
   return (
     <SafeAreaView>
       <View style={styles.map}>
         <View style={styles.toggle}>
-         <TouchableHighlight underlayColor="#FFFFFF" style={[styles.toggleBtn,selected=='Indoor' && styles.selectedBtn , selected=='Indoor' && styles.selectedIn ]} onPress={onPressButton2}>
-         <Text style={[styles.toggleText,selected=='Indoor'? styles.selectedBtn : {} ]}> Indoor </Text>
-         </TouchableHighlight>
-         <TouchableHighlight underlayColor="#FFFFFF" style={[styles.toggleBtn,selected=='Outdoor'&& styles.selectedBtn , selected=='Outdoor' && styles.selectedOut]}onPress={onPressButton}>
-         <Text style={[styles.toggleText,selected=='Outdoor'? styles.selectedBtn : {} ]}> Outdoor </Text>
-         </TouchableHighlight>
+          <TouchableHighlight
+            underlayColor="#FFFFFF"
+            style={[
+              styles.toggleBtn,
+              selected == 'Indoor' && styles.selectedBtn,
+              selected == 'Indoor' && styles.selectedIn,
+            ]}
+            onPress={onPressButton2}>
+            <Text
+              style={[
+                styles.toggleText,
+                selected == 'Indoor' ? styles.selectedBtn : {},
+              ]}>
+              {' '}
+              Indoor{' '}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            underlayColor="#FFFFFF"
+            style={[
+              styles.toggleBtn,
+              selected == 'Outdoor' && styles.selectedBtn,
+              selected == 'Outdoor' && styles.selectedOut,
+            ]}
+            onPress={onPressButton}>
+            <Text
+              style={[
+                styles.toggleText,
+                selected == 'Outdoor' ? styles.selectedBtn : {},
+              ]}>
+              {' '}
+              Outdoor{' '}
+            </Text>
+          </TouchableHighlight>
         </View>
 
-        <Button title="request wifi permissions" onPress={requestWifiPermission} />
-        <Button title="Testing" onPress={navigation.navigate("Testing")} />
+        <Button
+          title="request wifi permissions"
+          onPress={requestWifiPermission}
+        />
+        <Button
+          title="Testing"
+          onPress={() => {
+            navigation.navigate('Testing');
+          }}
+        />
 
         <View style={styles.navbar}>
-
-         <View style={styles.ic}>
-             <Icon color='#8E91A5' name="map" type="font-awesome" />
-             <Text style={styles.icText} > Explore </Text>
-         </View>
-
-        <View style={styles.ic}>
-         <Icon color='#8E91A5' name="person" />
-         <Text style={styles.icText} > Profile </Text>
-         </View>
-
-          <View style={styles.navigate}>
-            <Icon color='white' name="location-arrow" type="font-awesome"  />
-           </View>
+          <View style={styles.ic}>
+            <Icon color="#8E91A5" name="map" type="font-awesome" />
+            <Text style={styles.icText}> Explore </Text>
+          </View>
 
           <View style={styles.ic}>
-           <Icon color='#8E91A5' name="person" />
-           <Text style={styles.icText} > Profile </Text>
-            </View>
+            <Icon color="#8E91A5" name="person" />
+            <Text style={styles.icText}> Profile </Text>
+          </View>
 
-        <View style={styles.ic}>
-         <Icon color='#8E91A5' name="bars" type="font-awesome" />
-         <Text style={styles.icText} > More </Text>
+          <View style={styles.navigate}>
+            <Icon color="white" name="location-arrow" type="font-awesome" />
+          </View>
+
+          <View style={styles.ic}>
+            <Icon color="#8E91A5" name="person" />
+            <Text style={styles.icText}> Profile </Text>
+          </View>
+
+          <View style={styles.ic}>
+            <Icon color="#8E91A5" name="bars" type="font-awesome" />
+            <Text style={styles.icText}> More </Text>
           </View>
         </View>
       </View>
@@ -185,65 +217,65 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     backgroundColor: '#F0F1F3',
   },
-  navigate : {
-    width: 60 ,
-    height: 60 ,
-    backgroundColor:'#EA3924' ,
-    borderRadius : 100,
-    marginTop : -18,
-    elevation : 5,
-    paddingTop: 17 ,
+  navigate: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#EA3924',
+    borderRadius: 100,
+    marginTop: -18,
+    elevation: 5,
+    paddingTop: 17,
     shadowColor: '#000',
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     shadowOffset: {
       height: 0,
-      width: 0
-    }
+      width: 0,
+    },
   },
-  selectedBtn : {
-    backgroundColor : '#EA3924' ,
-    color : '#FFFFFF',
+  selectedBtn: {
+    backgroundColor: '#EA3924',
+    color: '#FFFFFF',
   },
-  selectedOut:{
-    borderBottomRightRadius:10 ,
-    borderTopRightRadius:10,
+  selectedOut: {
+    borderBottomRightRadius: 10,
+    borderTopRightRadius: 10,
   },
-  selectedIn : {
-    borderBottomLeftRadius:10,
-    borderTopLeftRadius:10,
+  selectedIn: {
+    borderBottomLeftRadius: 10,
+    borderTopLeftRadius: 10,
   },
-  toggle : {
-    alignSelf:'center' ,
-    marginTop:'10%',
-    height : '7%',
-    width:'50%',
-    backgroundColor : '#FFFFFF' ,
-    display:'flex' ,
-    flexDirection : 'row' ,
-    justifyContent:'space-between',
-    borderRadius:10,
+  toggle: {
+    alignSelf: 'center',
+    marginTop: '10%',
+    height: '7%',
+    width: '50%',
+    backgroundColor: '#FFFFFF',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 10,
   },
-  toggleBtn : {
-    color:'black',
-    padding:12,
-    width:'50%'
+  toggleBtn: {
+    color: 'black',
+    padding: 12,
+    width: '50%',
   },
-  toggleText : {
-    alignSelf:'center',
-    fontSize:15 ,
-    color:'#8E91A5' ,
-    fontWeight : 'bold',
-  } ,
+  toggleText: {
+    alignSelf: 'center',
+    fontSize: 15,
+    color: '#8E91A5',
+    fontWeight: 'bold',
+  },
   ic: {
     color: '#8E91A5',
     width: 60,
-    paddingTop:10,
+    paddingTop: 10,
   },
-  icText : {
-    color:'#8E91A5',
-    fontFamily : 'sans-serif' ,
-    alignSelf : 'center' ,
+  icText: {
+    color: '#8E91A5',
+    fontFamily: 'sans-serif',
+    alignSelf: 'center',
   },
   navbar: {
     backgroundColor: '#FFFFFF',
