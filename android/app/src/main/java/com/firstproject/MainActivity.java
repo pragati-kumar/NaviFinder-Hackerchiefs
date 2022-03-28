a63f15298d0271312c5e6c65a7cfdaf2b49f8da0
Utkarsh Gupta <utkarshgupta2504@gmail.com>
2022-03-28 09:14:31 +0530

    Renamed app

--- android/app/src/main/java/com/firstproject/MainActivity.java
+++ android/app/src/main/java/com/firstproject/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FirstProject";
+    return "SIH2022";
   }
 }
--- ios/SIH2022Tests/SIH2022Tests.m
+++ ios/SIH2022Tests/SIH2022Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FirstProjectTests : XCTestCase
+@interface SIH2022Tests : XCTestCase
 
 @end
 
-@implementation FirstProjectTests
+@implementation SIH2022Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
